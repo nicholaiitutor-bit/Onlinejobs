6d35f65df4beab035cef69a0a682f8988b8133a5
nicholaiitutor-bit <nicholaiitutor@gmail.com>
2026-06-03 08:11:20 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -37,3 +37,51 @@ function showView(view) {
         el.classList.add("fade-in");
     }
 }
+auth.onAuthStateChanged(async user => {
+
+```
+if (!user) return;
+
+currentUser = user;
+
+const userDoc =
+    await db.collection("users")
+    .doc(user.uid)
+    .get();
+
+currentUserRole = userDoc.data().role;
+
+document.getElementById("userEmail").innerText =
+    user.email;
+
+if (currentUserRole === "employer") {
+
+    document.getElementById("jobseekerMenu").style.display =
+        "none";
+
+    document.getElementById("employerMenu").style.display =
+        "block";
+
+    loadEmployerJobs();
+
+    showView("dashboard");
+
+}
+
+if (currentUserRole === "jobseeker") {
+
+    document.getElementById("employerMenu").style.display =
+        "none";
+
+    document.getElementById("jobseekerMenu").style.display =
+        "block";
+
+    loadJobs();
+
+    showView("findJobs");
+
+}
+```
+
+});
+
